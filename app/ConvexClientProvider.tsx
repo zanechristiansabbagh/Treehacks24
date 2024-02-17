02da450cc3cfc2fbdbbf0bb85a38c2ab9cb19a60
shumannxu <xu232002@gmail.com>
2024-02-16 21:17:31 -0800

    min convex files for next.js

---
+++ app/ConvexClientProvider.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { ReactNode } from "react";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithAuth0 } from "convex/react-auth0";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export default function ConvexClientProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <Auth0Provider
+      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN!}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID!}
+      authorizationParams={{
+        redirect_uri:
+          typeof window === "undefined" ? undefined : window.location.origin,
+      }}
+      useRefreshTokens={true}
+      cacheLocation="localstorage"
+    >
+      <ConvexProviderWithAuth0 client={convex}>
+        {children}
+      </ConvexProviderWithAuth0>
+    </Auth0Provider>
+  );
+}

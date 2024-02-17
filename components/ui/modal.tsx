3f292c746d9fd819d42f4f8d2dd291236dc2c1ff
zcsabbagh <zsabbagh@candidsocial.app>
2024-02-17 11:06:07 -0800

    new

---
+++ components/ui/modal.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface ModalProps {
+  children: React.ReactNode;
+  onClose: () => void;
+}
+
+const Modal: React.FC<ModalProps> = ({ children, onClose }) => {
+  return (
+    <div className="modal-backdrop">
+      <div className="modal-content">
+        {children}
+        <button onClick={onClose}>Close</button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;

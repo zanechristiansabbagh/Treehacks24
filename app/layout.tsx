a0084b269bb85fb632846fc6dc90611d712cdc09
mwirtz112 <mwirtz@candidsocial.app>
2024-02-16 22:37:25 -0800

    atttempt 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
+import ConvexClientProvider from "./ConvexClientProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <UserProvider>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ConvexClientProvider>{children}</ConvexClientProvider>
+        </body>
       </UserProvider>
     </html>
   );
---
+++ convex/tasks.js
@@ -0,0 +1,27 @@
+import { query } from "./_generated/server";
+import { v } from "convex/values";
+
+export const get = query({
+  args: {},
+  handler: async (ctx) => {
+    return await ctx.db.query("users").collect();
+  },
+});
+
+// export const getTask = query({
+//   args: {},
+//   handler: async (ctx, args) => {
+//     const profID = await ctx.db.get(args.taskId);
+//     return profID;
+//   },
+// });
+
+
+// import { query } from "./_generated/server";
+
+// export const get = query({
+//   args: {},
+//   handler: async (ctx) => {
+//     return await ctx.db.query("users").collect();
+//   },
+// });

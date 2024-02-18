7dd041ffada1620026782d80a8e52c25b9c2d045
mwirtz112 <mwirtz@candidsocial.app>
2024-02-18 04:02:53 -0800

    QR CODE CHANGE WITH EMAIL

--- app/joinClass/[parsedEmail]/page.tsx
+++ app/joinClass/[parsedEmail]/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { JoinClass } from "@/components/join-class"; // Assuming the path is correct based on the provided context
 
-export default function Page() {
+export default function Page({params}) {
   return (
     <div className="flex justify-center items-center h-screen">
-      <JoinClass />
+      <JoinClass parsedEmail={params.parsedEmail }/>
     </div>
   );
 }
--- components/qrcode.tsx
+++ components/qrcode.tsx
@@ -4,7 +4,20 @@ import { useQuery } from "convex/react";
 import { api } from "../convex/_generated/api";
 
 export function QRCode() {
-  const { user, error, isLoading } = useUser();
+  const { user } = useUser();
+  let email = ""
+  let parsedEmail = ""
+  if (user) {
+    email = user.email
+    console.log("EMAIL: ", email)
+    parsedEmail = email.replace(/\./g, "@URAD0G@")
+    console.log("PARSEDEMAIL: ", parsedEmail)
+  } else {
+    console.log("no user")
+  }
+
+
+  
 
   return (
     <div className="relative flex flex-col items-center justify-center h-screen w-full min-h-screen bg-black">
@@ -75,9 +88,9 @@ export function QRCode() {
       {/* <p className="text-lg text-white mt-4">Professor ID: {tasksId}</p> */}
 
       <div className="mt-8" style={{ width: "30vw", height: "30vw" }}>
-        <a href={`/joinClass`}>
+        <a href={`/joinClass/${parsedEmail}`}>
           <img
-            src={`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=https://taai-treehacks.vercel.app/joinClass`}
+            src={`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=https://taai-treehacks.vercel.app/joinClass/${parsedEmail}`}
             alt="QR Code"
             style={{ width: "100%", height: "100%" }}
           />
